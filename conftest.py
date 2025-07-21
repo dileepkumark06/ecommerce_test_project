2358ad067c1d6d16d7d497bc595f10ef6af7d244
Dileep Kumar K <sanjudileep.k12@gmail.com>
2025-07-21 20:38:49 +0530

    modified conftest.py

--- conftest.py
+++ conftest.py
@@ -2,8 +2,16 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture(scope="function")
-def browser():
-    driver = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("--browser")
+    if browser_name == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        driver = webdriver.Chrome()
+
     driver.maximize_window()
     yield driver
     driver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
